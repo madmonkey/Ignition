1b4f2799a4a6b1ef9ac2a9e1ef04a53b0b5572d4
Andrew Del Preore <adelpreore@gmail.com>
2013-05-03 07:38:28 -0400

    Initialization of sample project

---
+++ Ignition.Contracts/Class1.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Ignition.Contracts
+{
+    using ServiceStack.ServiceInterface;
+
+    public class Hello
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+    public class HelloService : Service
+    {
+        public object Execute(Hello request)
+        {
+            return new HelloResponse { Result = "Hello, " + request.Name };
+        }
+    }
+}
---
+++ Ignition/Global.asax.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Ignition
+{
+    using Funq;
+    //using Ignition.Contracts;
+    using ServiceStack.WebHost.Endpoints;
+
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //new HelloAppHost().Init();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+
+        //public class HelloAppHost : AppHostBase
+        //{
+        //    Tell Service Stack the name of your application and where to find your web services
+        //    public HelloAppHost() : base("Hello Web Services", typeof(HelloService).Assembly) { }
+
+        //    public override void Configure(Container container)
+        //    {
+        //        register user-defined REST-ful urls
+        //        Routes
+        //          .Add<Hello>("/hello")
+        //          .Add<Hello>("/hello/{Name}");
+        //    }
+        //}
+
+    }
+}
